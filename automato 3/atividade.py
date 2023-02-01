dd4e86ca515d0b41820fe51d55cf2136879e5898
João Pedro da Silva Inácio <93410032+Jpedrinacio@users.noreply.github.com>
2023-02-01 13:50:36 -0300

    Add files via upload

---
+++ automato 3/atividade.py
@@ -0,0 +1,33 @@
+def AFD(estado_atual, entrada):
+    transicoes = {
+        (0, 'a'): 0,
+        (0, 'b'): 2,
+        (1, 'b'): 3,
+        (2, 'a'): 4,
+        (3, 'a'): 3,
+        (3, 'b'): 1,
+        (4, 'b'): 4
+    }
+    if (estado_atual, entrada) in transicoes:
+        return transicoes[(estado_atual, entrada)]
+    else:
+        return None
+
+with open("entrada.txt", "r") as entrada_file:
+    entrada = entrada_file.read().strip()
+
+estado_inicial = 0
+estado_atual = estado_inicial
+for i in entrada:
+    estado_atual = AFD(estado_atual, i)
+    if estado_atual is None:
+        with open("saida.txt", "w") as saida_file:
+            saida_file.write("A entrada não é válida.")
+            print("A entrada não é válida.")
+        break
+else:
+    with open("saida.txt", "w") as saida_file:
+        saida_file.write("A entrada é válida.")
+        print("A entrada é válida.")
+
+print("resultado gravado no arquivo 'saida.txt'")
